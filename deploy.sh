cd2975caf72389781c9f5913adfc539014bab7aa
Sam Kerr <skerr@gitlab.com>
2019-09-11 12:57:48 -0400

    Update PS1

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ elif [[ ${uname} == "Darwin" ]]; then
 
     # Add custom Bash prompt
     echo <<-EOF >> ~/.profile
-    export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\] \[\033[33;1m\]\w\[\033[m\] (\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)) \n\$"
+    export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\] \[\033[33;1m\]\w\[\033[m\] (\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)) \n\$ "
     EOF
 fi
 
